8172b2b84f4ae8a0ce1b884b08e26412eccc30c0
Michael Gene Brockus <55331536+michael-gene-brockus@users.noreply.github.com>
2022-05-27 18:51:14 -0700

    Create main.py

---
+++ notebook/01-solve-me-first/main.py
@@ -0,0 +1,20 @@
+#
+# HackerRank folio
+# solved by Michael Gene Brockus
+#
+
+#
+# Should add the value of 'a' and 'b' and
+# return the sum of two.
+#
+# arg-list:
+#   -> a: first integer value being passed in
+#   -> b: second integer value being passed in
+#
+def solveMeFirst(a, b):
+    return a + b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
